13940b06cbac472d7b120f4a2baf1b1dc586b69a
aArntzen <158445429+aArntzen@users.noreply.github.com>
2024-05-27 11:18:43 +0200

    mail i api+

--- ProsjektSiste/api.js
+++ ProsjektSiste/api.js
@@ -1,11 +1,10 @@
 fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                data.forEach(user => {
-                    const markup = `<li>${user.name}</li>`;
-
-                    document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
-                })
-            })
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        data.forEach(user => {
+            const markup = `<li>${user.name},  Mail: ${user.email}</li>`;
+            document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
+        });
+    });
